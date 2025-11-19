438cf6ae4d833cdf980b5e504b316584e29a0302
Gabriel <211056000@aluno.unb.br>
2025-11-19 14:55:20 -0300

    Feat(Home): add home in prject

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ["name"]
+
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
+
+    const element2 = this.elemnt
+    const name2 = element2.value
+    console.log(`Hello, ${name2}!`)
   }
+
+
 }
+
